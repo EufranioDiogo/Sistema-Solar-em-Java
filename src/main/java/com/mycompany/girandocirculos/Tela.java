aa756eb0980f3ad9103b73e285f63500efc965c2
EufranioDiogo <eufraniodiogo5@gmail.com>
2020-11-16 20:46:31 +0100

    Best organized version

--- src/main/java/com/mycompany/girandocirculos/Tela.java
+++ src/main/java/com/mycompany/girandocirculos/Tela.java
@@ -6,7 +6,9 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Toolkit;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Ellipse2D;
+
 
 /**
  *
@@ -25,10 +27,11 @@ public class Tela extends JPanel implements Runnable {
     private float neptuneDegree = 0;
     private float plutoDegree = 0;
     private Point sunPosition = new Point(300, 100);
-    private Dimension sunSize = new Dimension(90, 90);
+    private Dimension sunSize = new Dimension(60, 60);
     private Point sunCenter;
     private Point sunBorder;
     private int marsXPosition;
+    private AffineTransform at;
     
     public Tela() {
         thread = new Thread(this, "Rotate Scare");
@@ -39,12 +42,12 @@ public class Tela extends JPanel implements Runnable {
     public void paintComponent(Graphics g){
         
             Graphics2D g2 = (Graphics2D) g;
-            Dimension screenSize = getSize();
+            Dimension windowSize = getSize();
             
             super.paintComponent(g2);
             
-            sunPosition.x = ((screenSize.width / 2) - (sunSize.width / 2));
-            sunPosition.y = ((screenSize.height / 2) - (sunSize.height / 2));
+            sunPosition.x = ((windowSize.width / 2) - (sunSize.width / 2));
+            sunPosition.y = ((windowSize.height / 2) - (sunSize.height / 2));
             sunCenter = new Point(this.sunPosition.x + (sunSize.width / 2), this.sunPosition.y + (sunSize.height / 2));
             sunBorder = new Point(sunCenter.x + (sunSize.width / 2), sunCenter.y + (sunSize.height / 2));
             
@@ -53,14 +56,18 @@ public class Tela extends JPanel implements Runnable {
             g2.fillOval(sunPosition.x, sunPosition.y, (int) sunSize.getWidth(), (int) sunSize.getHeight());
             
             // Mercury
+            at = new AffineTransform();
             g2.setColor(new Color(66, 66, 66));
-            g2.rotate(Math.toRadians(mercuryDegree), sunCenter.x, sunCenter.y);
+            at.rotate(Math.toRadians(mercuryDegree), sunCenter.x, sunCenter.y);
+            g2.setTransform(at);
             g2.fillOval((this.sunBorder.x) + 25, (this.sunCenter.y), 10, 10);
             
             
             // Venus
+            at = new AffineTransform();
             g2.setColor(new Color(224, 224, 38));
-            g2.rotate(Math.toRadians(venusDegree), sunCenter.x, sunCenter.y);
+            at.rotate(Math.toRadians(venusDegree), sunCenter.x, sunCenter.y);
+            g2.setTransform(at);
             g2.fillOval((this.sunBorder.x) + 50, (this.sunCenter.y), 15, 15);
             
             
@@ -68,46 +75,60 @@ public class Tela extends JPanel implements Runnable {
             
             
             //Mars
+            at = new AffineTransform();
             g2.setColor(Color.RED);
-            g2.rotate(Math.toRadians(marsDegree), sunCenter.x, sunCenter.y);
+            at.rotate(Math.toRadians(marsDegree), sunCenter.x, sunCenter.y);
+            g2.setTransform(at);
             g2.fillOval(this.sunBorder.x + 135, sunCenter.y, 12, 12);
             
             
             //Jupiter
+            at = new AffineTransform();
             g2.setColor(new Color(155, 142, 3));
-            g2.rotate(Math.toRadians(jupiterDegree), sunCenter.x, sunCenter.y);
-            g2.fillOval(this.sunBorder.x + 170, sunCenter.y, 50, 50);
+            at.rotate(Math.toRadians(jupiterDegree), sunCenter.x, sunCenter.y);
+            g2.setTransform(at);
+            g2.fillOval(this.sunBorder.x + 170, sunCenter.y, 40, 40);
             
             
             //Saturn
-            Ellipse2D saturn = new Ellipse2D.Double(this.sunBorder.x + 225, sunCenter.y, 45, 45);
+            at = new AffineTransform();
+            Ellipse2D saturn = new Ellipse2D.Double(this.sunBorder.x + 225, sunCenter.y, 35, 35);
             g2.setColor(new Color(191, 170, 118));
-            g2.rotate(Math.toRadians(saturnDegree), sunCenter.x, sunCenter.y);
+            at.rotate(Math.toRadians(saturnDegree), sunCenter.x, sunCenter.y);
+            g2.setTransform(at);
             g2.fill(saturn);
             
             
             //Uranus
+            at = new AffineTransform();
             g2.setColor(new Color(0, 153, 255));
-            g2.rotate(Math.toRadians(uranusDegree), sunCenter.x, sunCenter.y);
-            g2.fillOval(this.sunBorder.x + 300, sunCenter.y, 40, 40);
+            at.rotate(Math.toRadians(uranusDegree), sunCenter.x, sunCenter.y);
+            g2.setTransform(at);
+            g2.fillOval(this.sunBorder.x + 300, sunCenter.y, 30, 30);
             
             
             //Neptune
+            at = new AffineTransform();
             g2.setColor(new Color(0, 85, 255));
-            g2.rotate(Math.toRadians(neptuneDegree), sunCenter.x, sunCenter.y);
-            g2.fillOval(this.sunBorder.x + 345, sunCenter.y, 41, 41);
+            at.rotate(Math.toRadians(neptuneDegree), sunCenter.x, sunCenter.y);
+            g2.setTransform(at);
+            g2.fillOval(this.sunBorder.x + 345, sunCenter.y, 25, 25);
             
             
             //pluto
+            at = new AffineTransform();
             g2.setColor(new Color(255, 255, 255));
-            g2.rotate(Math.toRadians(plutoDegree), sunCenter.x, sunCenter.y);
+            at.rotate(Math.toRadians(plutoDegree), sunCenter.x, sunCenter.y);
+            g2.setTransform(at);
             g2.fillOval(this.sunBorder.x + 400, sunCenter.y, 5, 5);
             
             
             // Earth
+            at = new AffineTransform();
             g2.setColor(Color.BLUE);
-            Ellipse2D earth = new Ellipse2D.Double((int)(this.sunBorder.x) + 75, (int)(this.sunCenter.y), 15, 15);
-            g2.rotate(Math.toRadians(this.earthDegree), this.sunCenter.x, this.sunCenter.y);
+            Ellipse2D earth = new Ellipse2D.Double((int)(this.sunBorder.x) + 85, (int)(this.sunCenter.y), 15, 15);
+            at.rotate(Math.toRadians(this.earthDegree), this.sunCenter.x, this.sunCenter.y);
+            g2.setTransform(at);
             g2.fill(earth);
             
             // Earth moon
@@ -129,16 +150,16 @@ public class Tela extends JPanel implements Runnable {
     public void run(){
         try {
             while (true) {
-                this.mercuryDegree = (float) (this.mercuryDegree >= 360 ? 0 : 0.03 + this.mercuryDegree);
-                this.venusDegree = (float) (this.venusDegree >= 360 ? 0 : 0.04 + this.venusDegree);
-                this.earthDegree = (float) (this.earthDegree >= 360 ? 0 : 0.02 + this.earthDegree);
-                this.moonDegree = (float) (this.moonDegree >= 360 ? 0 : 0.1 + this.moonDegree);
-                this.marsDegree = (float) (this.marsDegree >= 360 ? 0 : 0.04 + this.marsDegree);
-                this.jupiterDegree = (float) (this.jupiterDegree >= 360 ? 0 : 0.05 + this.jupiterDegree);
-                this.saturnDegree = (float) (this.saturnDegree >= 360 ? 0 : 0.03 + this.saturnDegree);
-                this.uranusDegree = (float) (this.uranusDegree >= 360 ? 0 : 0.025 + this.uranusDegree);
-                this.neptuneDegree = (float) (this.neptuneDegree >= 360 ? 0 : 0.03 + this.neptuneDegree);
-                this.plutoDegree = (float) (this.plutoDegree >= 360 ? 0 : 0.01 + this.plutoDegree);
+                this.mercuryDegree += -1.5;
+                this.venusDegree += 1.0;
+                this.earthDegree += 0.5;
+                this.moonDegree += 0.8;
+                this.marsDegree += 0.5;
+                this.jupiterDegree += -0.4;
+                this.saturnDegree += 0.3;
+                this.uranusDegree += -0.25 ;
+                this.neptuneDegree += 0.2;
+                this.plutoDegree += -1;
 
                 Thread.sleep(24);
                 super.repaint();
